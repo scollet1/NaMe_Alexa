16060d18d503020c12457d5c0d6bdedb1690d5fd
Sam Collet <origamillamaa@gmail.com>
2018-08-11 23:23:47 -0700

    Update pu.sh

--- NaMe_Alexa_Node/lambda/custom/pu.sh
+++ NaMe_Alexa_Node/lambda/custom/pu.sh
@@ -4,7 +4,7 @@ then
 	rm "$file"
 fi
 zip -r lambdaFunc.zip . -x ./scripts/* ./firstnames/*
-aws lambda update-function-code --function-name aws-serverless-repository-alexaskillskitnodejsfact-1U5PTWHGH5FWO --zip-file fileb://./lambdaFunc.zip
+aws lambda update-function-code --function-name aws-serverless-repository-alexaskillskitnodejsfact-*** --zip-file fileb://./lambdaFunc.zip
 if [ -f "$file" ]
 then
         rm "$file"
